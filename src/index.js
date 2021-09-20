ccb06d8c5cb8831e540f06df36d3ef7797dee7b8
brianorora <brianorora@gmail.com>
2021-09-20 14:45:17 +0300

    add `populate` function to populate todo list

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
-const div = document.createElement('div');
-div.innerHTML = '<h1>Hello Webpack</h1>';
-document.body.appendChild(div);
+const todoContainerElement = document.getElementById('todo-container');
+
+const todoList = [
+  {
+    description: 'watch the dishes',
+    completed: true,
+    index: 0,
+  },
+  {
+    description: 'complete to do list project',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'watch "NO TIME TO DIE"',
+    completed: false,
+    index: 2,
+  },
+];
+
+const populate = () => {
+  todoList.forEach((item, index) => {
+    const todoElem = document.createElement('div');
+    todoElem.id = index;
+    todoElem.classList.add('todo-item');
+    const innerHtml = `
+    <input type="checkbox">
+    <div class="input-box">
+      <input type="text">
+    </div>
+    <div class="icon">
+      <i class="fas fa-ellipsis-v"></i>
+    </div>
+    `;
+
+    todoElem.innerHTML = innerHtml;
+    const inputBox = todoElem.querySelector('.input-box input');
+    inputBox.value = item.description;
+    const checkboxInput = todoElem.querySelector('input[type="checkbox"]');
+    checkboxInput.checked = item.completed;
+    todoContainerElement.appendChild(todoElem);
+  });
+};
+
+populate();
