36a0f1aac0973ec43f301822929457efda587e4c
brianorora <brianorora@gmail.com>
2021-09-20 22:44:53 +0300

    Add animation to refresh icon on updating localStorage

--- src/storage.js
+++ src/storage.js
@@ -1,11 +1,18 @@
 export const STORAGE_NAME = 'todo-list';
 
+let timeout;
+const refreshIcon = document.getElementById('refresh-todo-icon');
 export const updateStorage = (newList) => {
+  clearTimeout(timeout);
+  refreshIcon.style.opacity = 1;
   // update todo items indexes
   newList.forEach((item, idx) => {
     item.index = idx;
   });
   localStorage.setItem(STORAGE_NAME, JSON.stringify(newList));
+  timeout = setTimeout(() => {
+    refreshIcon.style.opacity = 0;
+  }, 400);
 };
 
 const createTodoList = () => {
