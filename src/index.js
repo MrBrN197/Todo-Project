c38574dc617015d96757822762ace0e69951db91
brianorora <brianorora@gmail.com>
2021-09-27 00:37:45 +0300

    Runn all code in `window.onload` event

--- src/index.js
+++ src/index.js
@@ -3,21 +3,23 @@ import './style.css';
 import { todoList } from './storage.js';
 import { onSubmit, createTodo, removeCompleted } from './addRemoveFunctions.js';
 
-const populate = () => {
-  todoList.data.forEach((item) => {
-    createTodo(item);
+window.addEventListener('load', () => {
+  const populate = () => {
+    todoList.data.forEach((item) => {
+      createTodo(item);
+    });
+  };
+
+  populate();
+
+  const addTodoBtn = document.getElementById('add-todo-btn');
+  const submitBtn = document.querySelector('form');
+  const clearCompletedBtn = document.getElementById('clear-completed-btn');
+
+  addTodoBtn.addEventListener('click', onSubmit);
+  submitBtn.addEventListener('submit', (e) => e.preventDefault() || onSubmit());
+
+  clearCompletedBtn.addEventListener('click', () => {
+    removeCompleted();
   });
-};
-
-populate();
-
-const addTodoBtn = document.getElementById('add-todo-btn');
-const submitBtn = document.querySelector('form');
-const clearCompletedBtn = document.getElementById('clear-completed-btn');
-
-addTodoBtn.addEventListener('click', onSubmit);
-submitBtn.addEventListener('submit', (e) => e.preventDefault() || onSubmit());
-
-clearCompletedBtn.addEventListener('click', () => {
-  removeCompleted();
 });
